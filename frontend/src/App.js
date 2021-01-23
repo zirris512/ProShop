eb133937d1cee7ef9c2256d4521aee197e278d22
Brent <mina512@live.com>
2021-01-22 21:09:02 -0600

    add home page cards

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import { Container } from "react-bootstrap";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import HomePage from "./pages/HomePage";
 
 const App = () => {
     return (
@@ -8,7 +9,7 @@ const App = () => {
             <Header />
             <main className="py-3">
                 <Container>
-                    <h1>Welcome To ProShop</h1>
+                    <HomePage />
                 </Container>
             </main>
             <Footer />
