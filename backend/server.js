b9812769c29665e753fba32cc9912f4e270c9d4d
Brent <mina512@live.com>
2021-02-19 22:00:22 -0600

    add err handler middleware

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import colors from "colors";
 import connectDB from "./config/db.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
 import productRoutes from "./routes/productRoutes.js";
 
@@ -12,6 +13,9 @@ connectDB();
 const app = express();
 
 app.use("/api/products", productRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
