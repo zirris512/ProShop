369d5cf2587da325ac0fb631036a8bc82c7155c4
Brent <mina512@live.com>
2021-06-19 17:42:13 -0500

    add auth token middleware

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { authUser } from "../controllers/userController.js";
+import { authUser, getUserProfile } from "../controllers/userController.js";
+import { protect } from "../middleware/authMiddleware.js";
 
 const router = Router();
 
 router.post("/login", authUser);
-
+router.route("/profile").get(protect, getUserProfile);
 
 export default router;
